4cb353d7870bef24ee71ee7c481b52cf3aa72dcc
nconnel4 <nconnel4@hotmail.com>
2021-03-28 19:29:54 -0400

    Add checks for greatest_increase and greatest_decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,6 +14,12 @@ def get_profit_values(data):
     total_change = 0
     previous_profit = None
 
+    # instantiate greatest profit increase and decrease
+    greatest_increase = {'date': '',
+                        'profit_change': 0}
+    greatest_decrease = {'date': '',
+                        'profit_change': 0}
+
     for row in data:
         current_profit = int(row['Profit/Losses'])
 
@@ -23,11 +29,21 @@ def get_profit_values(data):
         if not previous_profit is None:
             profit_change = get_profit_change(previous_profit, current_profit)
             total_change += profit_change
+
+
+            # check profit change values greater than previous greatest_increase
+            if profit_change > greatest_increase['profit_change']:
+                greatest_increase['date'] = row['Date']
+                greatest_increase['profit_change'] = profit_change
+            # check profit change values less than previous greatest_increase
+            elif profit_change < greatest_decrease['profit_change']:
+                greatest_decrease['date'] = row['Date']
+                greatest_decrease['profit_change'] = profit_change
         
         # set previous_profit equal to current_profit for next iteration
         previous_profit = current_profit
 
-    return net_profit, total_change
+    return net_profit, total_change, greatest_increase, greatest_decrease
 
 def get_profit_change(previous_month, current_month):
 
@@ -41,8 +57,8 @@ def __main__():
     total_months = len(bank_data)
 
     #calculate the net profits
-    net_profit, total_change = get_profit_values(bank_data)
-    print(total_months, net_profit, total_change)
+    net_profit, total_change, greatest_increase, greatest_decrease = get_profit_values(bank_data)
+    print(total_months, net_profit, total_change, greatest_increase, greatest_decrease)
 
 if __name__ == '__main__':
     __main__()
