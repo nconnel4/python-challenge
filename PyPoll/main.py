d05c98c2bf832fffdccf5de4729cb02c9626903b
nconnel4 <nconnel4@hotmail.com>
2021-03-29 09:17:05 -0400

    Add aggregate_election_data function
    Added function to take election data list and aggregate it together for
    the totals votes per candidate.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from Utility import csvreader, filewriter
 
+
 # get path of current folder
 script_path = Path(__file__).parent
 # get path to csv file
@@ -8,8 +9,31 @@ csv_path = script_path.joinpath('resources/election_data.csv')
 # set output file path
 out_path = script_path.joinpath('analysis/election_data_output.txt')
 
+
+def aggregate_election_data(data):
+
+    # instantiate dictionary
+    results = dict()
+
+    for row in data:
+        candidate = row['Candidate']
+
+        # increment vote count if candidate already in results set
+        if candidate in results:
+            results[candidate] += 1
+        # add key to results set if candidate doesn't exist
+        else:
+            results[candidate] = 1
+
+    return results
+
+
+
 def main():
-    pass
+    # read election data from file
+    election_data = csvreader.read_csv_to_list(csv_path)
+    election_aggregate = aggregate_election_data(election_data)
+
 
 if __name__ == '__main__':
     main()
