51b1e1546d12b6922a94d3e46cf36036e579e6ef
nconnel4 <nconnel4@hotmail.com>
2021-03-29 07:15:34 -0400

    Add main function

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,3 +7,9 @@ script_path = Path(__file__).parent
 csv_path = script_path.joinpath('resources/election_data.csv')
 # set output file path
 out_path = script_path.joinpath('analysis/election_data_output.txt')
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
