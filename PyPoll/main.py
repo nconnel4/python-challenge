b4effdabbe71122d1456d3af8009980537eaf678
nconnel4 <nconnel4@hotmail.com>
2021-03-29 09:52:51 -0400

    Add format_results_string function
    Added function to format the election results to the appropriate string
    for file creation and console output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -42,7 +42,7 @@ def calculate_vote_percentage(aggregate_data, total_votes):
     # loop through each candidate in the aggregate and calculate percentage of votes
     for candidate, votes in aggregate_data.items():
 
-        results[candidate] = votes / total_votes
+        results[candidate] = (votes / total_votes) * 100
 
     
     return results
@@ -64,6 +64,31 @@ def get_winner(aggregate_data):
     return winner['name']
 
 
+def format_results_string(vote_total, aggregate_votes, vote_percent, winner):
+    
+    # set initial output string
+    output_string = (
+        f'Election Results \n'
+        f'-------------------------- \n'
+        f'Total Votes: {vote_total} \n'
+        f'-------------------------- \n'
+    )
+
+    # add each candidate to output string
+    for candidate, votes in aggregate_votes.items():
+        output_string += f'{candidate}: {vote_percent.get(candidate):.3f}% ({votes}) \n'
+
+
+    # add winner to output string
+    output_string += (
+        f'-------------------------- \n'
+        f'Winner: {winner} \n'
+        f'-------------------------- \n'
+    )
+
+    return output_string
+    
+
 
 def main():
 
@@ -81,6 +106,12 @@ def main():
 
     # calculate winner
     winner_name = get_winner(election_aggregate)
+
+    # get results string
+    results_string = format_results_string(vote_total, election_aggregate, vote_percentage, winner_name)
+
+    # print results_string to file
+    print(results_string)
 
 
 
