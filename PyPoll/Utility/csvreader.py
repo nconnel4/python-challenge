a6630239ce9a8747d2a2ee80ffa34880218b3145
nconnel4 <nconnel4@hotmail.com>
2021-03-29 07:10:19 -0400

    Initial Commit

---
+++ PyPoll/Utility/csvreader.py
@@ -0,0 +1,18 @@
+import csv
+
+
+def read_csv_to_list(file_path, csv_delimiter = ','):
+    # reads csv file and returns a list of dictionaries with the key values set to header rows
+
+    csv_list = list()
+
+    with open(file_path) as file:
+        csv_list.extend([
+            {key: value for key, value in row.items()} 
+            for row in csv.DictReader(
+                file, 
+                delimiter = csv_delimiter, 
+                skipinitialspace = True)
+                ])
+
+    return csv_list
